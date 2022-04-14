8e4c82f143dffd98b9a921fa864c4ba527b6c42c
BLUE <hanhun0318@gmail.com>
2022-04-15 02:15:42 +0900

    kse_0414

--- 3st_week/1107/kimseongeun_1107.py
+++ 3st_week/1107/kimseongeun_1107.py
@@ -1,26 +1,46 @@
-n = int(input())
+N = int(input()) #이동하려고 하는 채널 N
+M = int(input()) #둘째 줄에는 고장난 버튼의 개수
+disabled_btn = []  # 고장난 버튼 리스트
 
-m = int(input())
-if m != 0:
-    arr = list(map(int, input().split()))
-else:
-    arr = []
-num = 100
+if M>0:
+    disabled_btn = input().split()
+result=abs(N-100) # 100부터 시작
+need_channels = [] # 조건에 맞는 채널만 담을 리스트
 
-ans = abs(n - 100)
-for i in range(1000001):
-    numArr = list(str(i))
-    flag = False
-    # 숫자를 누를 수 있는 지 검사
-    for k in numArr:
-        if int(k) in arr:
-            flag = True
+for i in range(0,1000001):
+    possible = True
+    for j in disabled_btn:
+        if j in str(i):
+            possible = False
             break
+    if possible:
+        need_channels.append(i)
 
-    if flag:
-        continue
-    # 해당 숫자를 누를 수 있다면 n까지 가는 방법 + 숫자를 누른횟수를 구해서 비교한다.
-    else:
-        ans = min(ans, abs(n - i) + len(str(i)))
+for k in need_channels: # 리스트에서 MIN 값 찾기
+    if abs(k-N) +len(str(k)) < result:
+        result = abs(k-N) +len(str(k))
 
-print(ans)
+print(result)
+
+
+# N = int(input()) #이동하려고 하는 채널 N
+# M = int(input()) #둘째 줄에는 고장난 버튼의 개수
+# disabled_btn = []  # 고장난 버튼 리스트
+#
+# def search_c(n, dis_btn):
+#     result=abs(n-100) #100부터 시작
+#     for i in range(0,1000001):
+#         possible=True
+#         for j in str(i):
+#             if j in dis_btn:
+#                 possible=False
+#                 break
+#         if possible:
+#             result=min(result, abs(n-i)+len(str(i)))
+#     return result
+#
+# if(M>0):
+#     disabled_btn = input().split()
+#     answer = search_c(N, disabled_btn)
+#
+# print(answer)
