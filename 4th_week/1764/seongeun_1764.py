aebb4ddbe1c8141a106eb898a13619c5bf278c18
BLUE <hanhun0318@gmail.com>
2022-04-29 02:16:29 +0900

    kse_0428

--- 4th_week/1764/seongeun_1764.py
+++ 4th_week/1764/seongeun_1764.py
@@ -2,7 +2,7 @@ N, M = map(int, input().split())
 #교집합을 찾는 문제
 nosee=set()
 nolisten=set()
-
+#rstrip : 인자로 전달된 문자를 String의 오른쪽에서 제거
 for i in range(N):
     nosee.add(input().rstrip())
 for i in range(M):
