77af787f95807ae67f1648e36d7ea6f3827325ec
HiBixby <fpssj@naver.com>
2022-05-10 00:06:08 +0900

    종이

---
+++ 6주차/1780/Bixby_1780.py
@@ -0,0 +1,20 @@
+def dp(r,c,n):
+    startWith=paper[r][c] #첫번째 원소가 무엇으로 시작하는지 저장
+
+    for i in range(r,r+n):
+        for j in range(c,c+n):
+            if paper[i][j]!=startWith: #같은숫자로 구성되지 않았으면
+                n//=3 #9등분하기 위해 size//3
+
+                for k in range(3):
+                   for l in range(3):
+                       dp(r+k*n, c+l*n, n) #잘린 9개 모두 재귀 호출
+                return
+    paperNumber[startWith+1]+=1
+    return
+
+n=int(input())
+paper=[list(map(int,input().split())) for _ in range(n)]
+paperNumber=[0]*3
+dp(0,0,n)
+print(*paperNumber,sep='\n')
