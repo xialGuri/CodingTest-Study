9793a22b85e6ed0e86de05c7e5695774d342b442
HiBixby <fpssj@naver.com>
2022-05-13 01:04:58 +0900

    회의실 배정

--- 6주차/1931/Bixby_1931_회의실배정.py
+++ 6주차/1931/Bixby_1931_회의실배정.py
@@ -1,10 +1,8 @@
 n=int(input())
 meetings=sorted([list(map(int,input().split())) for _ in range(n)], key=lambda x:(x[1],x[0]))
-
-cnt=0
-endTime=-1
-for i in range(n):
-    if meetings[i][0]>=endTime:
-        endTime=meetings[i][1]
+cnt=last=0
+for start,end in meetings:
+    if last<=start:
+        last=end
         cnt+=1
 print(cnt)
