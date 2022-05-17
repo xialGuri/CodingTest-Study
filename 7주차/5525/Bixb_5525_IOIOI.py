dea3b3b32dff9fed9d7f3e6c5287aa2af076a21a
HiBixby <fpssj@naver.com>
2022-05-17 19:35:06 +0900

    del

--- 7주차/5525/Bixb_5525_IOIOI.py
+++
@@ -1,7 +0,0 @@
-n=int(input())
-m=int(input())
-s=input()
-i=0
-while i<m:
-    if s[i:i+2]=='IO'
-    
--- 7주차/7569/Bixby_7569_토마토.py
+++
@@ -1,2 +0,0 @@
-m,n,h=map(int,input().split())
-box=[list(map(int,input().split())for _ in range(n*h))]
