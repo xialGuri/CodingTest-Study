e3c7dd2b882b2de5cad06191151c0a5cc9d678ce
HiBixby <fpssj@naver.com>
2022-06-20 01:26:41 +0900

    10주차1

---
+++ 10주차/1991/Bixby_1991_트리순회.py
@@ -0,0 +1,25 @@
+def preorder(root):
+    if root != '.':
+        print(root,end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root,end='')
+        inorder(tree[root][1])
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root,end='')
+n=int(input())
+tree={}
+for _ in range(n):
+    node,left,right=input().split()
+    tree[node]=[left,right]
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
--- 7주차/6064/bixby_6064.py
+++ 7주차/6064/bixby_6064.py
@@ -1,8 +1,9 @@
 for _ in range(int(input())):
-    m,n,x,y=map(int,input())
+    m,n,x,y=map(int,input().split())
     while x<=m*n:
         if (x-y)%n==0:
             print(x)
             break
         x+=m
-    print(-1)
+    else:
+        print(-1)
