470acf79b32325d26aa5fa10295d25ad0efb5931
HiBixby <fpssj@naver.com>
2022-06-22 20:03:12 +0900

    경로찾기(1)

--- 10주차/11403/Bixby_경로찾기.py
+++ 10주차/11403/Bixby_경로찾기.py
@@ -1,13 +1,8 @@
-#입력
 n=int(input())
-reachable=[[0 for _ in range(n)] for i in range(n)]
 graph=[list(map(int,input().split()))for _ in range(n)]
-#플로이드워셜
 for k in range(n):
     for i in range(n):
         for j in range(n):
-            if not reachable[i][j]:
-                reachable[i][j]=graph[i][j] or (graph[i][k] and graph[k][j])or (reachable[i][k] and reachable[k][j])
-#출력
-for column in reachable:
+            graph[i][j]|=(graph[i][k] and graph[k][j])
+for column in graph:
     print(*column)
